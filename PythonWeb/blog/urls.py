809eb1221e1572b9c05d632d48702ac7a8316e19
tvadiep <tvdiep.it@gmail.com>
2021-07-24 17:40:02 +0700

    wrote login, logout, generic view

--- PythonWeb/blog/urls.py
+++ PythonWeb/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.list, name = 'blog'),
-    path('<int:id>/', views.post),
+    path('', views.PostListView.as_view(), name = 'blog'),
+    path('<int:pk>/', views.PostDetailView.as_view()),
 ]
--- PythonWeb/home/urls.py
+++ PythonWeb/home/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index),
     path('contact/', views.contact, name = 'contact'),
     path('register/', views.register, name = 'register'),
-    path('login/', auth_views.LoginView, {'template': 'pages/login.html'}, name = 'login')
+    path('login/', auth_views.LoginView.as_view(template_name = 'pages/login.html'), name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page = '/'), name = 'logout')
 ]
